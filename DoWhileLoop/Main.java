44a6b5cebef7b122ada9f54ebdfe7d052e59ca40
Thanatoslayer6 <thanatoslayer6@gmail.com>
2022-11-08 20:58:53 +0800

    adding do while loop lesson

---
+++ DoWhileLoop/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String args[]) {
+        // int x = 1;
+        // int y = 3;
+        int i = 10;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 4);
+        // int numOfLoop = 0;
+        // do { 
+        //     x++;
+        //     y += 2;
+        //     numOfLoop++;
+        //     System.out.println("\nNumber of loop: " + numOfLoop);
+        //     System.out.println("The value of x: " + x);
+        //     System.out.println("The value of y: " + y);
+        //     System.out.println("The value of x + y: " + (x+y) + "\n\n");
+        // } while (x + y < 12);
+        // System.out.println(x + y);
+        // Do While Loop Example
+        // int x = 10;
+        // do {
+        //     x--;
+        //     System.out.println(x + " ");
+        // } while (x >= 2);
+    }
+}
+
