571d175351732501555fe2bc5fab36d71d6857bf
Thanatoslayer6 <thanatoslayer6@gmail.com>
2022-11-29 14:20:25 +0800

    Added some lesson

---
+++ ForLoop/Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+    public static void main(String args[]) {
+        /*
+        for (int i = -5; i <= -1; i++) {
+            for (int j = i; j <= -1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        * Outputs:
+        * -5-4-3-2-1
+        * -4-3-2-1
+        * -3-2-1
+        * -2-1
+        * -1
+        */
+        /*
+        for (char i = 'a'; i <= 'e'; i++) { // Outer rows
+            for (char  j = i;  j <= 'e'; j++) { // Inner cols
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+         * Outputs:
+         * abcde
+         * abcd
+         * abc
+         * ab
+         * a
+         *
+        */
+    }
+}
